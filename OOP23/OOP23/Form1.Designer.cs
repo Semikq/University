4c78058e06eb809167ce1f486919726ba909bf46
Semikq <usern1861@gmail.com>
2024-05-10 23:46:30 +0300

    OOP23

---
+++ OOP23/OOP23/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace OOP23
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(900, 350);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(301, 392);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 56);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Графік";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(435, 392);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 56);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Колір";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(900, 481);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "ООП23";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ OOP23/OOP23/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace OOP23
+{
+    public partial class Form1 : Form
+    {
+        private Graphics graphics;
+        private int offsetX;
+        private int offsetY;
+        private int ScaleGraphics;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            graphics = pictureBox1.CreateGraphics();
+            offsetX = pictureBox1.Width / 2;
+            offsetY = pictureBox1.Height / 2;
+            ScaleGraphics = 25;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            graphics.Clear(pictureBox1.BackColor);
+
+            ShowAxis();
+            ShowName();
+            ShowValues();
+
+            DrawX();
+            DrawY();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            colorDialog.ShowDialog();
+            pictureBox1.BackColor = colorDialog.Color;
+        }
+
+
+        private void ShowAxis()
+        {
+            Point firstX = new Point(0, offsetY);
+            Point secondX = new Point(offsetX * 2, pictureBox1.Height / 2);
+
+            Point firstY = new Point(offsetX, 0);
+            Point secondY = new Point(offsetX, offsetY * 2);
+
+            graphics.DrawLine(new Pen(Color.Black, 1f), firstX, secondX);
+            graphics.DrawLine(new Pen(Color.Black, 1f), firstY, secondY);
+        }
+
+        private void ShowName()
+        {
+            Font font = new Font("Times new Roman", 14, FontStyle.Bold);
+            SolidBrush brush = new SolidBrush(Color.Black);
+
+            Point x = new Point(offsetX * 2 - ScaleGraphics, offsetY - ScaleGraphics);
+            Point y = new Point(offsetX, 0);
+            graphics.DrawString("x", font, brush, x);
+            graphics.DrawString("y", font, brush, y);
+        }
+
+        private void ShowValues()
+        {
+            Font font = new Font("Arial", 10);
+            SolidBrush brush = new SolidBrush(Color.Black);
+
+            int xValue = -(offsetX) / ScaleGraphics;
+            for (int pixel = 0; xValue < offsetX * 2 / ScaleGraphics; pixel += ScaleGraphics, xValue++)
+            {
+                Point point = new Point(pixel, offsetY);
+                graphics.DrawString(xValue.ToString(), font, brush, point);
+            }
+
+            int yValue = -(offsetY) / ScaleGraphics;
+            for (int pixel = 0; yValue < offsetY * 2 / ScaleGraphics; pixel += ScaleGraphics, yValue++)
+            {
+                Point point = new Point(offsetX, pixel);
+                graphics.DrawString((-yValue).ToString(), font, brush, point);
+            }
+        }
+
+        private void DrawX()
+        {
+            Point[] points = new Point[1000];
+
+            for (int y = 0; y < points.Length; y++)
+            {
+                int x = (int)(y * y * Math.Sin((double)y / ScaleGraphics));
+                points[y] = new Point(x + offsetX, y);
+            }
+
+            graphics.DrawLines(new Pen(Color.Red, 3f), points);
+        }
+
+        private void DrawY()
+        {
+            Point[] points = new Point[1000];
+
+            for (int x = 0; x < points.Length; x++)
+            {
+                int y = (int)(x * Math.Pow(Math.Cos((double)x / ScaleGraphics), 2));
+                points[x] = new Point(x, y + offsetY);
+            }
+
+            graphics.DrawLines(new Pen(Color.Blue, 3f), points);
+        }
+    }
+}
